60fe650bf1519192e7dfb7da810a4222af972411
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-07 16:46:34 -0400

    Base Ui for events created

--- PiUi/src/app/app.module.ts
+++ PiUi/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule
