584b0b4e7ba49f221e93c5c31f26e874d352a49a
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-11 10:46:08 -0400

    moved db connection, added jquery

--- PiUi/src/app/app.component.ts
+++ PiUi/src/app/app.component.ts
@@ -7,25 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pi UI';
+
   ngOnInit() {
-
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-      host:"gradtechjnjdev.cxohlepxhfmz.us-east-1.rds.amazonaws.com",
-      user:"gradtechjnj_rw",
-      password:"gradt3chjnjd3v",
-      port:3306
-    });
-
-    connection.connect(function(err){
-      if(err){
-        alert("Error: " + err.stack);
-        return;
-      }
-      alert("Connected");
-    });
-
-    connection.end();
-
+    
   }
 }
--- PiUi/src/app/eventpanel/eventpanel.component.ts
+++ PiUi/src/app/eventpanel/eventpanel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {EVENTS} from '../mock-events';
+//import {CONNECTION} from '../connection';
 
 @Component({
   selector: 'app-eventpanel',
@@ -13,7 +14,25 @@ export class EventpanelComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var mysql = require('mysql');
+    alert("before create connect");
+    var connection = mysql.createConnection({
+      host:"gradtechjnjdev.cxohlepxhfmz.us-east-1.rds.amazonaws.com",
+      user:"gradtechjnj_rw",
+      password:"gradt3chjnjd3v"
+    });
 
+    alert("after create connect");
+    connection.connect(function(err) {
+      if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+      }
+
+      console.log('Connected to database.');
+    });
+
+    connection.end();
   }
 
 }
