84f8abc4f9889873729ec7bc3ac39676b3b20eaf
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-15 00:50:41 -0400

    changed some css

--- PiUi/src/app/event.service.ts
+++ PiUi/src/app/event.service.ts
@@ -13,11 +13,11 @@ const httpOptions = {
 @Injectable({providedIn: 'root'})
 export class EventService {
 
-  private tempEventUrl = 'http://34.239.113.101:8080/demo/temps';
-  private motionEventUrl = 'http://34.239.113.101:8080/demo/motions';
+  //private tempEventUrl = 'http://34.239.113.101:8080/demo/temps';
+  //private motionEventUrl = 'http://34.239.113.101:8080/demo/motions';
 
-  //private tempEventUrl = 'http://localhost:8080/demo/temps';
-  //private motionEventUrl = 'http://localhost:8080/demo/motions';
+  private tempEventUrl = 'http://localhost:8080/demo/temps';
+  private motionEventUrl = 'http://localhost:8080/demo/motions';
 
   constructor(private http: HttpClient) { }
 
