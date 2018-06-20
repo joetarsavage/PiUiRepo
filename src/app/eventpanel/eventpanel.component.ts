ce6466f11b7f40d2752daf1d413c383db2098c7e
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-20 10:15:13 -0400

    added auto refresh to angular

--- src/app/eventpanel/eventpanel.component.ts
+++ src/app/eventpanel/eventpanel.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { interval } from 'rxjs';
 
 import {TempEvent} from '../temp.event';
 import {MotionEvent} from '../motion.event';
@@ -16,6 +17,8 @@ export class EventpanelComponent implements OnInit {
 
   motionEvents: MotionEvent[];
 
+
+
   @Output() clicked = new EventEmitter<boolean>();
 
   constructor(private eventService:EventService){}
@@ -23,13 +26,14 @@ export class EventpanelComponent implements OnInit {
   click(bool: boolean){
     this.clicked.emit(bool);
   }
+  onRefresh(){
+    this.getEvents();
+  }
 
   ngOnInit() {
+    const refreshTimer = interval(2000);
     this.getEvents();
-    var self = this;
-    $(document).ready(function(){
-      
-    });
+    refreshTimer.subscribe(n => this.onRefresh());
   }
 
   getEvents(): void{
