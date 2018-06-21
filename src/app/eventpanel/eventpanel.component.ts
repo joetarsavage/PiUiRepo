6bae369ffe0ba41dcd2168c3f30479e9b455a11e
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-21 10:11:57 -0400

    highlights selected event, unhighlights after refresh

--- src/app/eventpanel/eventpanel.component.ts
+++ src/app/eventpanel/eventpanel.component.ts
@@ -5,6 +5,8 @@ import {TempEvent} from '../temp.event';
 import {MotionEvent} from '../motion.event';
 import {EventService} from '../event.service';
 
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-eventpanel',
@@ -15,24 +17,28 @@ export class EventpanelComponent implements OnInit {
 
   motionEvents: MotionEvent[];
 
-
-
   @Output() clicked = new EventEmitter<boolean>();
 
   constructor(private eventService: EventService){}
 
   click(bool: boolean){
     this.clicked.emit(bool);
-
   }
   onRefresh(){
     this.getEvents();
   }
-
+  //set global variable and reset css on refresh based on id
   ngOnInit() {
+    var self = this;
+
     const refreshTimer = interval(2000);
     this.getEvents();
     refreshTimer.subscribe(n => this.onRefresh());
+
+    $(document).on("click",".eventsListItem",function(){
+      $('li').removeClass("selected");
+      $(this).addClass("selected");
+    });
   }
 
   getEvents(): void{
