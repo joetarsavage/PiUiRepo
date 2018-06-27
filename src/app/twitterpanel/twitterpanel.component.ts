1c81fe11902b4d5c64356d4aa6210c6ee1e66c1c
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-27 16:41:40 -0400

    removed twitter stuff, trying stock stuff

---
+++ src/app/twitterpanel/twitterpanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twitterpanel',
+  templateUrl: './twitterpanel.component.html',
+  styleUrls: ['./twitterpanel.component.css']
+})
+export class TwitterpanelComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
