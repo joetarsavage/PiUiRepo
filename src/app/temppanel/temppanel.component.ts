1130848180da76b7291a982883002fb9ef760b4e
rbyrne-entech <ryan.byrne@entechconsulting.com>
2018-06-27 11:03:34 -0400

    Default view changed to one day

--- src/app/temppanel/temppanel.component.ts
+++ src/app/temppanel/temppanel.component.ts
@@ -19,11 +19,12 @@ export class TemppanelComponent implements OnInit, AfterViewInit {
   allTempData: number[];
   allHumidityData: string[];
   allDates: string[];
+  prettyDates: string[];
   numOneDayBack: number;
   numOneWeekBack: number;
-  chartType = 'line';
+  chartType: any = 'line';
   xlabels;
- // chartOptions = {'showXLabels': 5};
+  chartOptions: any = {'showXLabels': 8};
   chartColor = [{ // grey
     backgroundColor: 'rgba(148,159,177,0.2)',
     borderColor: 'rgba(148,159,177,1)',
@@ -55,12 +56,17 @@ export class TemppanelComponent implements OnInit, AfterViewInit {
     this.allTempData = temp.map(dat =>  dat.temp );
     this.allHumidityData = temp.map(dat => dat.humidity);
     this.allDates = temp.map(dat => dat.occurredTs);
-    this.xlabels = this.allDates.slice();
-    this.temperatureData = [{data: this.allTempData.slice(), label: 'Temperature °F'},
-      {data: this.allHumidityData.slice(), label: 'Humidity %'}];
+    // .map( date => new Date(date).getDate().toString())
+
+
     this.currentDate = new Date();
     this.numOneDayBack = this.numIterationsBack(new Date(new Date().setDate(new Date().getDate() - 1)));
     this.numOneWeekBack = this.numIterationsBack(new Date(new Date().setDate(new Date().getDate() - 5)));
+    this.xlabels = this.allDates.slice(this.numOneDayBack,
+      this.allDates.length - this.numOneDayBack);
+    this.temperatureData = [{data: this.allTempData.slice(this.numOneDayBack,
+        this.allTempData.length - this.numOneDayBack), label: 'Temperature °F'},
+      {data: this.allHumidityData.slice(this.numOneDayBack, this.allHumidityData.length - this.numOneDayBack), label: 'Hunmidity %'}];
   }
   displayAll() {
     this.chart.labels = this.allDates.slice();
