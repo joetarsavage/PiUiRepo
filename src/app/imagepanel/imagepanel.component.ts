fec970aa43a55577da4197aab3458d117db9a519
Joe Tarsavage <joseph.tarsavage@entechconsulting.com>
2018-06-28 10:16:39 -0400

    code now receives stock data and stores it in arrays

--- src/app/imagepanel/imagepanel.component.ts
+++ src/app/imagepanel/imagepanel.component.ts
@@ -17,11 +17,11 @@ export class ImagepanelComponent implements OnInit {
   ngOnInit() {
     var self = this;
     $(document).ready(function(){
-      $('h2').bind("DOMSubtreeModified",function(){
+      $('#imageH2').bind("DOMSubtreeModified",function(){
         $("#loading").show();
         $("#pic").hide();
         $.ajax({
-          url: 'http://18.204.21.212:8080/demo/getImageById/' + self.id,
+          url: 'http://54.210.23.150:8080/demo/getImageById/' + self.id,
           //url: 'http://localhost:8080/demo/getImageById/' + self.id,
           success:function(data){
             $("#loading").hide();
--- src/app/twitterpanel/twitterpanel.component.ts
+++ src/app/twitterpanel/twitterpanel.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
+import {BaseChartDirective} from 'ng2-charts';
 
 @Component({
   selector: 'app-twitterpanel',
   templateUrl: './twitterpanel.component.html',
   styleUrls: ['./twitterpanel.component.css']
 })
-export class TwitterpanelComponent implements OnInit{
+export class TwitterpanelComponent implements OnInit, AfterViewInit{
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
+  times: [];
+  prices: [];
+
+  @Input('stockDate') dateTime: string;
+  date: string;
+  time: string;
+
+
 
   constructor() { }
 
   ngOnInit() {
+    var self = this;
+    $(document).ready(function() {
+      $('#stockDateH2').bind("DOMSubtreeModified",function(){
+        var dateAndTime = self.dateTime.split(' ');
+        self.date = dateAndTime[0].replace(/-/g,'');
+        var timeArr = dateAndTime[1].split(":");
+        self.time = timeArr[0].concat(":",timeArr[1]);
+        $.ajax({
+          url: 'https://api.iextrading.com/1.0/stock/jnj/chart/date/' + self.date,
+          //url: 'http://localhost:8080/demo/getImageById/' + self.id,
+          success:function(data){
+            var arrTimes = [];
+            var arrPrices = [];
+            var lastPrice = 0;
+            $.each(data, function(i,stock){
+              arrTimes.push(stock.minute);
+              if(stock.average == -1){
+                arrPrices.push(lastPrice);
+              }else{
+                arrPrices.push(stock.average);
+                lastPrice = stock.average;
+              }
+            });
+            self.times = arrTimes;
+            self.prices = arrPrices;
+          }
+        });
+      });
+    });
   }
-
 }
